bd76538baee33a8c04a4e21741490bc1567f712a
kutukov2000 <kutukov16.2005@gmail.com>
2023-12-24 17:45:34 +0200

    feat: add Nomatch page ✨

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ContainerDefault from "./containers/default/ContainerDefault.tsx";
 import { Route, Routes } from 'react-router-dom';
 import CategoriesListPage from './components/categories/list/CategoriesListPage.tsx';
+import NoMatch from './components/pages/Nomatch.tsx';
 
 const App: React.FC = () => {
     return (
       <Routes>
         <Route path='/' element={<ContainerDefault/>}>
           <Route index element={<CategoriesListPage/>} />
+          <Route path='*' element={<NoMatch/>} />
         </Route>
       </Routes>
     );
